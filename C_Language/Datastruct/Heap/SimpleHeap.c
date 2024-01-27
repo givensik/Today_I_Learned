74a7c1a490e4491ab40c38c01980e3432a692c38
givensik <kjs4072@icloud.com>
2024-01-27 22:53:13 +0900

    2024/1/27 Simple Heap Finish

--- C_Language/Datastruct/Heap/SimpleHeap.c
+++ C_Language/Datastruct/Heap/SimpleHeap.c
@@ -8,10 +8,10 @@ void HeapInit(Heap * ph){
 }
 
 int HIsEmpty(Heap * ph){
-	if(ph->numOfData == 0){
-		return TRUE;
-	}else{
+	if(ph->numOfData > 0){
 		return FALSE;
+	}else{
+		return TRUE;
 	}
 }
 
@@ -27,18 +27,25 @@ int GetRChildIDX(int idx){
 }
 
 // 우선순위가 높은 자식 노드의 인덱스를 출력
-// 자식노드가 없음 -> 0, 자식 노드가 하나 -> 
+// 자식노드가 없음 -> 0, 자식 노드가 하나 -> 1
 int GetHiPriChildIDX(Heap * ph, int idx){
-	// 왼쪽 자식 노드의 인덱스가 힙의 마지막 노드의 인덱스보다 크면
+	// printf("numOfData = %d\n", ph->numOfData);
+	// printf("부모 노드 인덱스 = %d일때\n",idx);
+	// printf("왼쪽 자식 노드의 우선 순위 = %d\n",ph->heapArr[GetLChildIDX(idx)].pr);
+	// printf("오른쪽 자식 노드의 우선 순위 = %d\n",ph->heapArr[GetLChildIDX(idx)+1].pr);
+	// 왼쪽 자식 노드의 인덱스가 힙의 마지막 노드의 인덱스보다 크면 안됨
 	if(GetLChildIDX(idx) > ph->numOfData){
 		return 0;
-	}else if(GetLChildIDX(idx) == ph->numOfData){
-		return 1;
+	}else if(GetLChildIDX(idx) == ph->numOfData){ // 왼쪽 자식의 인덱스가 마지막 노드의 인덱스랑 같으면 왼쪽 자식 노드만 있는거 인데..? 삭제한거면 마지막 노드가 올라간 상태로 가정해야하는 거 아닌가?
+		// printf("노드 하나라 왼쪽 노드\n");
+		return GetLChildIDX(idx);
 	}else{// 자식 노드 둘 다 있으면
 		// 오른쪽 자식 노드의 우선순위가 높을 때(pr값이 작아야 우선순위가 높다.)
 		if(ph->heapArr[GetLChildIDX(idx)].pr > ph->heapArr[GetRChildIDX(idx)].pr ){
+			// printf("오른쪽이 우선순위 높음\n");
 			return GetRChildIDX(idx);
 		}else{
+			// printf("왼쪽이 우선순위 높음\n");
 			return GetLChildIDX(idx);
 		}
 	}
@@ -46,9 +53,9 @@ int GetHiPriChildIDX(Heap * ph, int idx){
 
 
 void HInsert(Heap * ph, HData data, Priority pr){
-	printf("HINSERT %d\n", data);
+	// printf("HINSERT %d\n", data);
 	int idx = ph->numOfData + 1;
-	HeapElem nelem = {pr, data};
+	HeapElem nelem = {data, pr};
 	// idx가 1이 아닐때 = 1이면 루트 노드임
 	while(idx != 1){
 		// 새로 추가하는 원소의 우선순위가 부모보다 높을 때
@@ -65,12 +72,14 @@ void HInsert(Heap * ph, HData data, Priority pr){
 	ph->heapArr[idx] = nelem;
 	ph->numOfData += 1;		
 }
+
 HData HDelete(Heap * ph){
-	printf("Delete \n");
+	
+	// printf("Delete \n");
 	// 지울 데이터 값 -> 루트 노드의 데이터
 	HData retData = (ph->heapArr[1]).data;
 
-	// 루트 노드에 마지막 노드를 저장
+	// 임시로 마지막 노드를 저장
 	HeapElem lastElem = ph->heapArr[ph->numOfData]; 
 	
 	int parentIdx = 1;
@@ -79,11 +88,19 @@ HData HDelete(Heap * ph){
 	// 루트 노드부터 우선 순위가 높은 자식 노드랑 비교 교환? 을 하지 않고 우선 순위만 비교하면서 인덱스 값의 값을 찾아감
 	while(1){
 		childIdx = GetHiPriChildIDX(ph, parentIdx);
-		printf("lastnode = %c -> pr : %d\n ChildIdx = %c -> pr : %d\n",lastElem.data, lastElem.pr,(ph->heapArr[childIdx]).data,(ph->heapArr[childIdx]).pr  );
-		// 마지막 노드의 우선순위가 자식 노드보다 더 낮을 때 -> 멈추기
-		if(lastElem.pr > (ph->heapArr[childIdx]).pr){
+		// printf("lastnode = %c -> pr : %d\n ChildIdx = %c -> pr : %d\n", lastElem.data, lastElem.pr,(ph->heapArr[childIdx]).data,(ph->heapArr[childIdx]).pr  );
+		
+		// if(parentIdx == ph->numOfData){
+		// 	printf("parentIdx = %d, numOfData = %d\n -> 멈춤",parentIdx, ph->numOfData);
+		// 	break;
+		// }
+		
+		// 마지막 노드의 우선순위가 자식 노드보다 더 높을 때 -> 멈추기
+		if(lastElem.pr <= (ph->heapArr[childIdx]).pr){
+			// printf("lastnode pr = %d > 자식노드 pr =  %d\n -> 우선순위 낮으니까 멈춤",lastElem.pr, (ph->heapArr[childIdx]).pr);
 			break;
 		}
+		// 마지막 노드의 우선순위가 더 높을 때 -> 부모 노드에 자식 노드를 넣고 부모노드로 올라간 자식 노드의 인덱스를 부모인덱스로 설정
 		ph->heapArr[parentIdx] = ph->heapArr[childIdx];
 		parentIdx = childIdx;
 	}
--- C_Language/Datastruct/Heap/SimpleHeapMain.c
+++ C_Language/Datastruct/Heap/SimpleHeapMain.c
@@ -4,19 +4,30 @@
 int main(void) {
 	Heap heap;
 	HeapInit(&heap);
-
+	int i = 1;
 	HInsert(&heap, 'A', 1);
 	HInsert(&heap, 'B', 2);
 	HInsert(&heap, 'C', 3);
 	printf("%c\n", HDelete(&heap));
 
+	// for(i = 1; i<heap.numOfData + 1 ; i ++){
+	// 	printf("i = %d, %c\n", i, heap.heapArr[i].data);
+	// }
 	HInsert(&heap, 'A', 1);
 	HInsert(&heap, 'B', 2);
 	HInsert(&heap, 'C', 3);
 	printf("%c\n", HDelete(&heap));
-
-	while(!HIsEmpty(&heap)) 
+	// for(i = 1; i<heap.numOfData + 1 ; i ++){
+	// 	printf("i = %d, %c\n", i, heap.heapArr[i].data);
+	// }
+	// printf("%d\n", HIsEmpty(&heap));
+	while(!HIsEmpty(&heap)){
 		printf("%c\n", HDelete(&heap));
+	}
 
 	return 0;
 }
+/*	1 2 3 4 5 6
+	A B C C B 
+	1 2 3 3 2 
+*/
